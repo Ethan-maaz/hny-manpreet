3986596c4e470f8ccb0505ad7eef481a374cc604
Maaz Ajaz <103590871+Ethan-maaz@users.noreply.github.com>
2023-12-27 19:25:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentPage = 1;
-const totalPages = 4; // Replace with the actual total number of pages
+const totalPages = 13; // Replace with the actual total number of pages
 
 // Map page numbers to content types ('photo' or 'video')
 const contentTypeMap = {
@@ -8,10 +8,10 @@ const contentTypeMap = {
     3: 'video',
     4: 'photo',
     5: 'photo',
+    6: 'photo',
+    7: 'photo',
     // Add more entries as needed
 };
-
-// ...
 
 async function loadBookContent(page) {
     try {
@@ -31,21 +31,27 @@ async function loadBookContent(page) {
             const videoResponse = await fetch(`video_${page}.mp4`);
             const videoData = await videoResponse.blob();
             const videoUrl = URL.createObjectURL(videoData);
-            document.getElementById('pageImage').src = ''; // Clear image source
+            
+            // Hide the image and its text for video content
+            document.getElementById('pageImage').style.display = 'none';
+            document.getElementById('pageImageText').style.display = 'none';
+            
+            // Show the video container and set its source
             document.getElementById('pageVideo').src = videoUrl;
             document.getElementById('pageVideo').style.display = 'block';
-            // Hide "Page Image" text for video
-            document.getElementById('pageImageText').style.display = 'none';
         } else if (contentType === 'photo') {
             // Load image for specified pages
             const imageResponse = await fetch(`image_${page}.jpg`);
             const imageData = await imageResponse.blob();
             const imageUrl = URL.createObjectURL(imageData);
-            document.getElementById('pageVideo').src = ''; // Clear video source
-            document.getElementById('pageVideo').style.display = 'none';
+            
+            // Show the image and its text for photo content
             document.getElementById('pageImage').src = imageUrl;
-            // Show "Page Image" text for photo
+            document.getElementById('pageImage').style.display = 'block'; // Corrected line
             document.getElementById('pageImageText').style.display = 'block';
+            
+            // Hide the video container for photo content
+            document.getElementById('pageVideo').style.display = 'none';
         }
 
         document.getElementById('currentPage').innerText = `Page ${page}`;
@@ -54,9 +60,6 @@ async function loadBookContent(page) {
         document.getElementById('textContent').innerText = 'Error loading content.';
     }
 }
-
-
-
 
 function nextPage() {
     if (currentPage < totalPages) {
